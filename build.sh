31c3df397c7b5ceb4fe62e465a78dc7c2c41d385
c2j <c2j@CJJs-MacBookPro.local>
2015-12-24 06:39:29 +0800

    Initial base image

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+#if [ ! -f settings ]; then
+#  echo "Error loading configuration; settings file missing."
+#  exit 1
+#fi
+
+#source ./settings
+
+for b in base gtm gtm_proxy coordinator datanode; do
+  cd ${b}/
+  docker build -t postgres-x2_${b} .
+  cd ..
+done
